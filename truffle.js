2fe8540e74111e13bbddf75b905808e2cdbb2ce1
tomgtbst <tom.gt@foxmail.com>
2019-09-06 20:34:35 +0800

    Create truffle.js

---
+++ truffle.js
@@ -0,0 +1,25 @@
+//var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "";
+
+module.exports = {
+  networks: {
+    // development: {
+    //   provider: function() {
+    //     return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
+    //   },
+    //   network_id: '*',
+    //   gas: 9999999
+    // }
+    development:{
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+      gas: 9999999
+    }
+  },
+  compilers: {
+    solc: {
+      version: "^0.4.24"
+    }
+  }
+};
